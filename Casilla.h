18b9a24bf40dea0077243f6a63e074ede878e257
melsandoval <m.san43@gmail.com>
2014-03-24 22:25:36 -0600

    Create Casilla.h

---
+++ Casilla.h
@@ -0,0 +1,15 @@
+#ifndef CASILLA_H
+#define CASILLA_H
+#includa <stdbool.h>
+
+typedef struct Casilla{
+  char paredArriba;
+  char paredAbajo;
+  char paredDerecha;
+  char paredIzquierda;
+  char visitado;
+  bool visita
+  int valor;
+}Casilla;
+
+#endif
